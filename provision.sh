42d5d0becf2ac86ce4fc1adc364794a99ddbbf4e
Y.Kentaro <y.ken.studio@gmail.com>
2014-03-31 16:36:19 +0900

    Update Mroonga-4.01-1 with MySQL 5.6.17-1

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-MYSQL_VER="5.6.16-1"
-MROONGA_VER="4.00-2"
+MYSQL_VER="5.6.17-1"
+MROONGA_VER="4.01-1"
 
 yum -y groupinstall 'Development Tools'
 yum -y install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
